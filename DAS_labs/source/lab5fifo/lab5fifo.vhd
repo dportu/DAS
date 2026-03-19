bb5567868e41f7bf1bf04a54181e949a837a09e9
dportu <126913208+dportu@users.noreply.github.com>
2026-03-19 18:08:10 +0100

    Refactor FIFO implementation in lab5fifo.vhd

--- DAS_labs/source/lab5fifo/lab5fifo.vhd
+++ DAS_labs/source/lab5fifo/lab5fifo.vhd
@@ -1,19 +1,3 @@
----------------------------------------------------------------------
---
---  Fichero:
---    lab5.vhd  12/09/2023
---
---    (c) J.M. Mendias
---    Diseño Automático de Sistemas
---    Facultad de Informática. Universidad Complutense de Madrid
---
---  Propósito:
---    Laboratorio 5: Loopback con FIFO
---
---  Notas de diseño:
---
----------------------------------------------------------------------
-
 library ieee;
 use ieee.std_logic_1164.all;
 
@@ -52,6 +36,19 @@ architecture syn of lab5 is
   signal displayEns  : std_logic_vector (3 downto 0);
   signal displayBins : std_logic_vector (15 downto 0);
   
+  component fifo_generator_0
+  port (
+    clk   : in  std_logic;
+    srst  : in  std_logic;
+    din   : in  std_logic_vector(7 downto 0);
+    wr_en : in  std_logic;
+    rd_en : in  std_logic;
+    dout  : out std_logic_vector(7 downto 0);
+    full  : out std_logic;
+    empty : out std_logic
+  );
+end component;
+  
 begin
 
   rstSynchronizer : synchronizer
@@ -66,12 +63,15 @@ begin
     generic map ( FREQ_KHZ => FREQ_KHZ, BAUDRATE => BAUDRATE )
     port map ( clk => clk, rst => rstSync, dataRdy => dataRdyRx, data => dataRx, RxD => RxD );
 
-  fifo : fifoQueue
-    generic map ( WIDTH => 8, DEPTH => 16 )
-    port map ( clk => clk, rst => rstSync, wrE => dataRdyRx, dataIn => dataRx, rdE => dataRdyTx, dataOut => dataTx, numData => numData, full => full, empty => empty );
+--  fifo : fifoQueue
+--    generic map ( WIDTH => 8, DEPTH => 16 )
+--    port map ( clk => clk, rst => rstSync, wr_en => dataRdyRx, din => dataRx, rd_en => dataRdyTx, dout => dataTx, numData => numData, full => full, empty => empty );
 
-  -- Leemos de la FIFO (y transmitimos) cuando el transmisor no está ocupado,
-  -- hay datos en la FIFO y la transmisión está habilitada.
+    fifo: fifo_generator_0
+        port map(clk => clk, srst => rstSync, wr_en => dataRdyRx, din => dataRx, rd_en => dataRdyTx, dout => dataTx, full => full, empty => empty);
+
+  -- Leemos de la FIFO (y transmitimos) cuando el transmisor no estÃ¡ ocupado,
+  -- hay datos en la FIFO y la transmisiÃ³n estÃ¡ habilitada.
   dataRdyTx <= not busy and not empty and TxEnSync;
    
   transmitter: rs232transmitter 
@@ -81,27 +81,29 @@ begin
   displayEns  <= "110" & en;
   displayBins <= dataRx(7 downto 4) & dataRx(3 downto 0) & "0000" & fifoStatus;
 
-  -- Display derecho: F si llena, E si vacía
+  -- Display derecho: F si llena, E si vacÃ­a
   fifoStatus <= X"F" when full='1' else X"E";
   
-  -- El display derecho solo se habilita cuando la FIFO está llena o vacía
+  -- El display derecho solo se habilita cuando la FIFO estÃ¡ llena o vacÃ­a
   en <= full or empty;
 
   -- LEDs: tantos encendidos como datos haya en la FIFO
-  numDataDecoder:
-  process( numData, full )
-  begin
+  -- numDataDecoder:
+    --   process( numData, full )
+    --   begin
+    --     leds <= (others => '0');
+    --     if full = '1' then
+    --       leds <= (others => '1');
+    --     else
+    --       for i in 0 to 15 loop
+    --         if i < to_integer(unsigned(numData)) then
+    --           leds(i) <= '1';
+    --         end if;
+    --       end loop;
+    --     end if;
+    --   end process;
+    
     leds <= (others => '0');
-    if full = '1' then
-      leds <= (others => '1');
-    else
-      for i in 0 to 15 loop
-        if i < to_integer(unsigned(numData)) then
-          leds(i) <= '1';
-        end if;
-      end loop;
-    end if;
-  end process;
   
   displayInterface : segsBankRefresher
     generic map ( FREQ_KHZ => FREQ_KHZ, SIZE => 4 )
